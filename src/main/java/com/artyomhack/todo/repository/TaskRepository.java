e1fed2c75cd9a94ed6d66a4e2959a1a3869440de
xakimullin2@gmail.com <xakimullin2@gmail.com>
2023-08-25 19:30:35 +0300

    removed gitignored files

--- src/main/java/com/artyomhack/todo/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.artyomhack.todo.repository;
-
-import com.artyomhack.todo.entity.TaskEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends CrudRepository<TaskEntity, Long> {
-}
